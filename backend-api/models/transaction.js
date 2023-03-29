2eaa8508c07f02e15ac7e2afec4a552f198fadd2
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-29 23:29:46 +0200

    creating model for database

---
+++ backend-api/models/transaction.js
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+
+const transactionSchema = new Schema({
+
+    name: {type: "string", required: true},
+    datetime: {type: "date", required: true},
+    description: {type: "string", required: true},
+
+}); 
+
+export const transactionModel = model('Transaction', transactionSchema);
